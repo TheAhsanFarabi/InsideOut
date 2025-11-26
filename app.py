dee6acc1084652ed1149bed6c5e5eaf669cc4b75
Minhaz Fuad <97557100+MinhazFuad@users.noreply.github.com>
2025-11-27 00:55:43 +0600

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.markdown("""
 <style>
     /* Blurred background effect - Using a placeholder URL */
     .stApp {
-        background: url("https://images.unsplash.com/photo-1518066000714-cdcd825c673e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+        background: url("https://images.unsplash.com/photo-1585507252242-11fe632c26e8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
         background-size: cover;
         background-attachment: fixed;
         background-position: center;
